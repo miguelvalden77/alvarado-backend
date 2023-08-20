de93023f20a9ab2e033472da34da34c4d3cd3046
miguelvalden <miguelvalden77@gmail.com>
2023-08-20 23:08:51 +0200

    fix owner route

--- routes/owner.routes.js
+++ routes/owner.routes.js
@@ -8,17 +8,20 @@ router.post("/allTransactions", isAuth, async (req, res, next) => {
 
     const { state, page, ventas } = req.body
     const pagina = page * ventas
+    console.log({ state })
 
     try {
-        if (!state) {
-            const transactions = await Transaction.find({ status: "succeeded" }).sort({ createdAt: -1 }).skip(pagina - ventas).limit(pagina)
+        if (!state || state == "Todos") {
             const total = await Transaction.count()
+            const transactions = await Transaction.find({ status: "succeeded" }).sort({ createdAt: -1 }).skip(pagina - ventas).limit(ventas)
             res.json({ transactions, total })
             return
         }
 
-        const transactions = await Transaction.find({ status: "succeeded", state: state }).sort({ createdAt: -1 }).skip(pagina - 10).limit(pagina)
-        res.json(transactions)
+        const transactions = await Transaction.find({ status: "succeeded", state: state }).sort({ createdAt: -1 }).skip(pagina - ventas).limit(ventas)
+        const total = await Transaction.find({ status: "succeeded", state: state }).count()
+        console.log({ transactions, total })
+        res.json({ transactions, total })
         return
     }
     catch (err) {
